a2f23b38ad4b68c163b015e129c0e607dd2f0dc5
RaccoonDML <2684813584@qq.com>
2023-04-17 19:50:31 +0800

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # This branch (fsc) is for FSC147 dataset
+This branch provides baseline code for few-shot object counting task.
+
+ VGG16Trans model is adopted for counting. 
+ 
+ FSC147 dataset is released by [LearningToCountEverything](https://arxiv.org/pdf/2104.08391.pdf).
+
+For more related works about class-agnostic counting(CAC), please refer this repository [Awesome-Class-Agnostic-Counting](https://github.com/RaccoonDML/Awesome-Class-Agnostic-Counting)
+
+## to run
+1. fork or clone this repository
+2. pip install -r requirements.txt
+3. download dataset, then put [images_384_VarV2](https://drive.google.com/file/d/1ymDYrGs9DSRicfZbSCDiOu0ikGDh5k6S/view?usp=sharing) and  [gt_density_map_adaptive_384_VarV2](https://archive.org/details/FSC147-GT) in ./datasets/FSC
+4. sh run.sh
--- run.sh
+++ run.sh
@@ -1,8 +1,2 @@
-# train on QNRF
-python train.py --tag CHSNet-qnrf --no-wandb --device 0 --max-noisy-ratio 0.05 --max-weight-ratio 0.5 --scheduler cosine --dcsize 4 --batch-size 8 --lr 4e-5 --data-dir ../DATASET/QNRF-trainfull-test-dmapfix15 --val-start 200 --val-epoch 5
-# train on SHA
-python train.py --tag CHSNet-sha --no-wandb  --device 1 --max-noisy-ratio 0.10 --max-weight-ratio 1.0 --scheduler cosine --dcsize 2 --batch-size 8 --lr 4e-5 --data-dir ../DATASET/SHA-train-test-dmapfix15      --val-start 200 --val-epoch 5
-# train on SHB
-python train.py --tag CHSNet-shb --no-wandb  --device 2 --max-noisy-ratio 0.05 --max-weight-ratio 1.0 --scheduler cosine --dcsize 4 --batch-size 8 --lr 4e-5 --data-dir ../DATASET/SHB-train-test-dmapfix15      --val-start 200 --val-epoch 5
-
-
+# train VGG16Trans model on FSC
+python train.py --tag fsc-baseline --no-wandb --device 0 --scheduler step --step 400 --dcsize 8 --batch-size 8 --lr 4e-5 --val-start 100 --val-epoch 10
